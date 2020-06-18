bea501d56d1b12dc1137444e3d1e2b1594b7eb9d
Dhruv Raj Garg <dhruv.rajgarg@ymail.com>
2020-06-18 15:49:53 +0530

    Complete

--- index.js
+++ index.js
@@ -2,13 +2,16 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const todoCount = document.querySelector(".count");
+//ToDo Count
+var count = document.querySelectorAll(".todo-list .todo").length;
+todoCount.innerText = count;
 
 //Event Listners
 todoButton.addEventListener("click", addTodo);
-todoList.addEventListener("click" , deleteCheck);
+todoList.addEventListener("click", deleteCheck);
 //Functions
 function addTodo(event) {
-
   //Prevent form from Submiting
   event.preventDefault();
   //ToDo Div
@@ -16,6 +19,10 @@ function addTodo(event) {
   todoDiv.classList.add("todo");
 
   const newTodo = document.createElement("li");
+  if (todoInput.value == "") {
+    window.alert("Input Cannot be Empty");
+    return;
+  }
   newTodo.innerText = todoInput.value;
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
@@ -35,17 +42,21 @@ function addTodo(event) {
   //Append to list
   todoList.appendChild(todoDiv);
   todoInput.value = "";
+  count = document.querySelectorAll(".todo-list .todo").length;
+  todoCount.innerText = count;
 }
 
-function deleteCheck(e){
-    const item = e.target;
-    // delete todo 
-    if(item.classList[0] === "trash-btn"){
-        const todo = item.parentElement;
-        todo.remove();
-    }
-    if(item.classList[0] === "complete-btn"){
-        const todo = item.parentElement;
-        todo.classList.toggle('completed');
-    }
+function deleteCheck(e) {
+  const item = e.target;
+  // delete todo
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.remove();
+    count = document.querySelectorAll(".todo-list .todo").length;
+    todoCount.innerText = count;
+  }
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
